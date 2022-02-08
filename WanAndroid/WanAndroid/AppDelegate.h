86bcff38bf01e08bcde9ba819a969c1f0a009e6b
Roger <burnings0506@gmail.com>
2022-02-08 16:45:16 +0800

    Initial project

---
+++ WanAndroid/WanAndroid/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WanAndroid
+//
+//  Created by Roger on 2022/2/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
---
+++ WanAndroid/WanAndroid/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  WanAndroid
+//
+//  Created by Roger on 2022/2/8.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[UIViewController alloc] init];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
